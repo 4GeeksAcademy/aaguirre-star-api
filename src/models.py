3eb10682e7b0063b17d3e3c26d948da721ec88f1
aaguirre <alejandra36a@gmail.com>
2024-09-17 10:43:05 +0000

    primera copia

--- src/models.py
+++ src/models.py
@@ -2,18 +2,58 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    username= db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    firstname = db.Column(db.String(20), unique=False, nullable=False)
+    lastname= db.Column(db.String(20), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
-    def serialize(self):
+    def __repr__(self): 
+        return '<User %r>' % self.id
+
+    def serialize(self): #convierte la instancia de modelo en un diccionario p. 
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "firstname": self.firstname, 
+            "lastname": self.lastname, 
+            "username": self.username
+            
         }
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+
+    def __repr__(self): 
+        return '<User %r>' % self.id
+
+    def serialize(self): #convierte la instancia de modelo en un diccionario p. 
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+class Personajes(db.Model):
+    id = db.Column(db.Integer,primary_key=True )
+    name = db.Column(db.String(20), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+        
+class Favorito (db.Model):
+    id = db.Column(db.Integer,primary_key=True )
+
+
+
+    
+    
